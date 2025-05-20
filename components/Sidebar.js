d9d65adee55de26cc93e65b12c9ebb300c6b810e
Clubmd24 <kieranj1989@sky.com>
2025-05-20 21:43:10 +0000

    fix: correct Dev Portal link path to /dev/projects

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,7 +4,7 @@ export function Sidebar() {
     <aside className="w-64 bg-[var(--color-primary)] text-white h-screen p-4 space-y-4">
       <Link href="/admin/users"><a className="block rounded-lg p-2 hover:bg-[var(--color-primary-light)]">Users</a></Link>
       <Link href="/admin/settings"><a className="block rounded-lg p-2 hover:bg-[var(--color-primary-light)]">Settings</a></Link>
-      <Link href="/developers/projects"><a className="block rounded-lg p-2 hover:bg-[var(--color-primary-light)]">Dev Portal</a></Link>
+      <Link href="/dev/projects"><a className="block rounded-lg p-2 hover:bg-[var(--color-primary-light)]">Dev Portal</a></Link>
     </aside>
   );
 }
