254a64c987be5110e0e1ec19a51544bc00b70bf4
Clubmd24 <kieranj1989@sky.com>
2025-06-26 03:37:10 +0200

    feat: apply theme from localStorage

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,11 @@
 import '../styles/globals.css';
+import { useEffect } from 'react';
+
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const stored = localStorage.getItem('theme');
+    document.documentElement.classList.toggle('dark', stored === 'dark');
+  }, []);
+
   return <Component {...pageProps} />;
 }
