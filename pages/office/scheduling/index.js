eac146a73df292ddd4ae833f3a69e56ac76faade
Clubmd24 <kieranj1989@sky.com>
2025-07-10 22:17:41 +0000

    Fix import path casing: remove .jsx extension from SchedulingCalendar

--- pages/office/scheduling/index.js
+++ pages/office/scheduling/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import OfficeLayout from '../../../components/OfficeLayout';
-import SchedulingCalendar from '../../../components/SchedulingCalendar.jsx';
+// pages/office/scheduling/index.js
+
+import React from 'react'
+import OfficeLayout from '../../../components/OfficeLayout'
+import SchedulingCalendar from '../../../components/SchedulingCalendar'
 
 const SchedulingPage = () => (
   <OfficeLayout>
     <h1 className="text-xl font-semibold">Scheduling</h1>
     <SchedulingCalendar />
   </OfficeLayout>
-);
+)
 
-export default SchedulingPage;
+export default SchedulingPage
