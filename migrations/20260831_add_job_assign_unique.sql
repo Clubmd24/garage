d81c9814c418fcc9e94c9e97559d14f4fdcaac62
Clubmd24 <kieranj1989@sky.com>
2025-07-24 03:03:03 +0200

    Add cleanup to job assignment unique constraint migration

--- migrations/20260831_add_job_assign_unique.sql
+++ migrations/20260831_add_job_assign_unique.sql
@@ -1,2 +1,6 @@
+DELETE ja1 FROM job_assignments ja1
+JOIN job_assignments ja2
+  ON ja1.job_id = ja2.job_id AND ja1.id > ja2.id;
+
 ALTER TABLE job_assignments
   ADD CONSTRAINT uq_job_assignments_job UNIQUE (job_id);
